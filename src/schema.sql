f6da1578e2b62f8670fcc56f59e99c9430923ed5
Ilya Ordin <oxilor@gmail.com>
2023-03-26 20:36:09 +0300

    Run tests sequentially

--- src/schema.sql
+++ src/schema.sql
@@ -1,3 +1,15 @@
+CREATE OR REPLACE FUNCTION truncate_tables() RETURNS void AS $$
+  DECLARE
+    tables CURSOR FOR
+      SELECT schemaname, tablename FROM pg_tables
+      WHERE NOT schemaname = ANY(ARRAY['pg_catalog', 'information_schema']);
+  BEGIN
+    FOR tbl IN tables LOOP
+      EXECUTE 'TRUNCATE TABLE ' || quote_ident(tbl.schemaname) || '.' || quote_ident(tbl.tablename) || ' RESTART IDENTITY CASCADE';
+    END LOOP;
+  END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE tasks (
   id serial PRIMARY KEY,
   created_at timestamptz NOT NULL DEFAULT NOW(),
--- src/test-files/setupTests.ts
+++ src/test-files/setupTests.ts
@@ -7,10 +7,10 @@ import createSeeder, { Seed } from '../utils/createSeeder';
 import createServer from '../utils/createServer';
 
 interface TestContext {
-  client: TestClient;
   pool: DatabasePool;
   seed: Seed;
   server: http.Server;
+  client: TestClient;
 }
 
 // eslint-disable-next-line import/prefer-default-export, import/no-mutable-exports
@@ -20,34 +20,23 @@ const JEST_WORKER_INDEX = Number(process.env.JEST_WORKER_ID) - 1;
 const PORT = 4000 + JEST_WORKER_INDEX;
 const URL = `http://localhost:${PORT}/graphql`;
 
-let pool: DatabasePool;
-
 beforeAll(async () => {
-  pool = await createPool();
+  const pool = await createPool();
+  ctx = {
+    pool,
+    seed: createSeeder(pool),
+    server: await createServer(pool, PORT),
+    client: new TestClient({ url: URL }),
+  }
 });
 
 afterAll(async () => {
-  // Close the connection
-  await pool.end();
-});
-
-beforeEach(async () => {
-  // Start a new transaction
-  await pool.query(sql.unsafe`START TRANSACTION`);
-
-  const client = new TestClient({ url: URL });
-  const seed = createSeeder(pool);
-  const server = await createServer(pool, PORT);
-
-  ctx = { client, pool, seed, server };
-});
-
-afterEach(async () => {
-  // Rollback the transaction
-  await ctx.pool.query(sql.unsafe`ROLLBACK`);
-
-  // Close the server
   await new Promise((resolve) => {
     ctx.server.close(resolve);
   });
+  await ctx.pool.end();
+});
+
+afterEach(async () => {
+  await ctx.pool.query(sql.unsafe`SELECT truncate_tables()`);
 });
